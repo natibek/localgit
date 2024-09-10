004fe1032faa9a002bb8da9c8b1d1e2e0b843699
natibek <nwtbekele@gmail.com>
2024-09-10 05:25:02 -0500

    update workflow

--- publish_package.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-toplevel=$(git rev-parse --show-toplevel --path-format=absolute)
-
-# This publishes checks-superstaq to PyPI.
-cd "$toplevel"
-python -m build
-twine upload dist/* -u __token__ -p "$PYPI_API_KEY"
